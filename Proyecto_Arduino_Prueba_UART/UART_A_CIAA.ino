c83e0609952e730d52ce4f000454373da0bb2b18
Lucas Escribano <lucasescribano@hotmail.com.ar>
2019-03-17 18:39:35 -0300

    Actualizando lista de Pendientes...

---
+++ Proyecto_Arduino_Prueba_UART/UART_A_CIAA.ino
@@ -0,0 +1,48 @@
+/*
+  Serial Event example
+
+  When new serial data arrives, this sketch adds it to a String.
+  When a newline is received, the loop prints the string and clears it.
+
+  A good test for this is to try it with a GPS receiver that sends out
+  NMEA 0183 sentences.
+
+  NOTE: The serialEvent() feature is not available on the Leonardo, Micro, or
+  other ATmega32U4 based boards.
+
+  created 9 May 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/SerialEvent
+*/
+
+char byteRcv;
+int numero;
+unsigned int tiempo_set;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  Serial1.begin(115200);
+  attachInterrupt(19, serialInterrupt, CHANGE);
+}
+
+void loop() {
+
+      Serial1.print("Hi, i am Arduino!!! El número es: ");
+      Serial1.print(numero);
+      Serial1.print("\r\n");
+      numero++;
+
+    delay(30000);
+
+ }
+
+void  serialInterrupt()
+{
+  char c = Serial1.read();
+  Serial.print(c);
+  Serial.print("Hola");
+}
